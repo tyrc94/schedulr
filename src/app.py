424be23f264bcabdc5797a68b131b3a830a56204
Tyrell Carew <tcarew21@gmail.com>
2018-07-24 21:20:25 +0100

    something

--- src/app.py
+++ src/app.py
@@ -1,3 +1,11 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, send_from_directory
 
 app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def something():
+    return send_from_directory('client/src/', 'index.html')
+
+@app.route('/Index.js', methods = ['GET'])
+def something_else():
+    return send_from_directory('client/build/', 'Index.js')
