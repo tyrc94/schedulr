492da4461fa7487a8ae4afc6a281ddf9ca84f6c6
Tyrell Carew <tcarew21@gmail.com>
2018-07-24 23:34:55 +0100

    added user lookup, database commands

--- src/app.py
+++ src/app.py
@@ -1,11 +1,26 @@
-from flask import Flask, request, redirect, render_template, send_from_directory
+from flask import Flask, request, redirect, render_template, send_from_directory, jsonify
+import json
+
+import db.db
 
 app = Flask(__name__)
+db.db.init_db()
 
 @app.route('/', methods = ['GET'])
-def something():
+def server_html():
     return send_from_directory('client/src/', 'index.html')
 
+
 @app.route('/Index.js', methods = ['GET'])
-def something_else():
+def server_js():
     return send_from_directory('client/build/', 'Index.js')
+
+
+@app.route('/user/<user_id>', methods = ['GET'])
+def get_user(user_id):
+    user_info = db.db.user_details(user_id)
+    return jsonify(user_info)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ src/db/db.py
@@ -0,0 +1,95 @@
+import sqlite3
+from flask import g, Flask
+app = Flask(__name__)
+
+DATABASE = './db/database.db'
+
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+
+    db.row_factory = make_dicts
+    return db
+
+
+def mutate_db(query, args=()):
+    db = get_db()
+    db.execute(query, args)
+    db.commit()
+    return True
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def check_login(username, password):
+    user = query_db('SELECT * FROM User WHERE email = ? AND password = ?',
+                [username, password], one=True)
+    if user is None:
+        return False
+    else:
+        return user
+
+
+def register(username, password, forename, surname):
+    try:
+        user = mutate_db("INSERT INTO User (email, password, forename, surname) VALUES (?,?,?,?)",
+                    [username, password, forename, surname])
+        return True
+    except Exception as e:
+        return False
+
+
+def user_details(user_id):
+    return query_db('SELECT * FROM users WHERE id = ?', [user_id])
+
+
+def create_task(name, description, priority):
+    return mutate_db("INSERT INTO tasks (name, description, priority_id) VALUES (?, ?, ?)", 
+                [name, description, priority])
+
+
+def update_task(task_id, name = None, description = None, priority = None):
+    params = {'name': name, 'description': description, 'priority': priority}
+    dct = {k: v for k, v in params if v is not None}
+    for arg in dct:
+        mutate_db("UPDATE tasks SET ? = ? WHERE task_id = ?", [arg, dct[arg], task_id])
+
+
+def delete_task(task_id):
+    return mutate_db("DELETE FROM tasks WHERE id = ?", task_id)
+
+
+def add_collaborators(task_id, user_ids):
+    for user_id in user_ids:
+        mutate_db("INSERT INTO collaborators (user_id, task_id) VALUES (?, ?)", [task_id, user_ids])
+
+
+def remove_collaborators(task_id, user_ids):
+    for user_id in user_ids:
+        mutate_db("DELETE FROM collaborators WHERE task_id = ? AND user_id = ?", [task_id, user_ids])
